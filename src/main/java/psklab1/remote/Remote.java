918ab1a377dccac69f0fa28422432bfa2dd165af
Justinas <justinasidk@gmail.com>
2019-05-29 12:06:31 +0300

    Fixed long call

--- src/main/java/psklab1/remote/Remote.java
+++ src/main/java/psklab1/remote/Remote.java
@@ -7,19 +7,25 @@ import javax.ejb.Asynchronous;
 import javax.enterprise.context.ApplicationScoped;
 import java.io.Serializable;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @ApplicationScoped
 @Asynchronous
 public class Remote implements Serializable, Remot {
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Futureable
     public Future<Integer> updateInRemote() {
-        try {
-            Thread.sleep(8000);
-        } catch (InterruptedException e) {
-        }
-        final Integer remoteSystemID = new Random().nextInt(100);
-        return new AsyncResult<>(remoteSystemID);
+        return executor.submit(() -> {
+            try {
+                Thread.sleep(8000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return new Random().nextInt(100);
+        });
     }
 }
